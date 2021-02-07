63b7f5b560e376e01b293895a323896004ae4f37
Veronika Takács <roni9764@gmail.com>
2021-02-07 15:22:23 +0100

    Initialize Codewars repo

---
+++ Codewars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Codewars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
